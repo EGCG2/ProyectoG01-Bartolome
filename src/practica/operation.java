2a7a590256defbb7fb38c9bb02f278e62461c4ef
Bartolome <barmardom@gmail.com>
2016-11-11 09:45:43 +0100

    Comiteo inicial del proyecto

---
+++ src/practica/operation.java
@@ -0,0 +1,28 @@
+package practica;
+
+public class operation {
+	
+	 public static int suma(int numero1, int numero2){
+		    
+		    int resultado = 0;
+		    
+		    //Sumamos los dos numeros
+		    resultado = numero1 + numero2;
+		    
+		    //Retornamos el valor
+		    return resultado;
+	}
+	 
+	 public static int resta(int numero1, int numero2){
+		    
+		    int resultado = 0;
+		    
+		    //Sumamos los dos numeros
+		    resultado = numero1 - numero2;
+		    
+		    //Retornamos el valor
+		    return resultado;
+	}
+	
+	
+}
---
+++ src/practica/test.java
@@ -0,0 +1,32 @@
+package practica;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class test {
+
+	   @Test
+	   public void sumaPositivos() {
+	      System.out.println("Sumando dos números positivos ...");
+	      
+	      Integer primerNumero = 2;
+	      Integer segundoNumero = 3;
+	      Integer resultado = operation.suma(primerNumero, segundoNumero);
+	      
+	      assertTrue(resultado==5);
+	   }
+	   
+	   
+	   
+	   @Test
+	   public void restaPositivos() {
+	      System.out.println("Restando dos números positivos ...");
+	      
+	      Integer primerNumero = 3;
+	      Integer segundoNumero = 2;
+	      Integer resultado = operation.suma(primerNumero, segundoNumero);
+	      
+	      assertTrue(resultado==1);
+	   }
+
+	} 
